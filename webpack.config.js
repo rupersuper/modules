525f1bdda4d280d6ab4f66145ba351e3125c55d7
rupersuper <rupersuper@mail.ru>
2024-05-12 02:13:57 +0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
